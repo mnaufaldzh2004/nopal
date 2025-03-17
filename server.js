18500be044266239171ecc7305c5fd05acf77aca
MBaihaqi <baihaqi1401@student.ub.ac.id>
2025-03-17 21:10:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -49,5 +49,5 @@ app.get('/home', (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000; 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
